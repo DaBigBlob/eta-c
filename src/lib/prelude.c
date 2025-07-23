9b673a7e7ff71afaa560acb085da04cd3829fc1a
Person <localboxcrox@gmail.com>
2025-07-23 00:30:28 -0400

    stuff

--- src/lib/prelude.c
+++ src/lib/prelude.c
@@ -11,20 +11,13 @@ typedef unsigned int uint;
 typedef unsigned long ulong;
 
 typedef byte* memp; // mem pointer // not void* to make *arithmetic easier
-typedef ulong memsz; // mem size
-
-typedef memp status; // error/success propagation
-#define OKSTATUS 0
-#define ok(stat) stat == OKSTATUS //^ if status s = 0 then no error
-#define err(stat) stat != OKSTATUS
-//^ it always points to a /0 terminated string
-
+typedef ulong memsz; // mem sized
 
 typedef list(char) string;
 
 typedef enum _tag_bool {
     true = 1,
     false = !true
-} __attribute__((__packed__)) bool;
+} bool; // maybe even? __attribute__((__packed__)) bool;
  
 #endif //SRC_LIB_PRELUDE_C
--- src/sys/targets/_requirements.c
+++ src/sys/targets/_requirements.c
@@ -1,16 +1,18 @@
 #ifndef SRC_SYS_TARGETS__REQUIREMENTS_C
 #define SRC_SYS_TARGETS__REQUIREMENTS_C
 
+// _requirements -> targets -> target_guards -> internal use
+
 #include "../../lib/prelude.c"
 
-status _target_unpack_args(uint *ret_argc_p, list(string) * ret_argv_p, string *ret_err_p);
-status _target_exit(uint code);
+void _target_unpack_args(uint *ret_argc_p, list(string) * ret_argv_p, string *ret_err_p);
+void _target_exit(uint code);
 
-status _target_openf(const string path, uint oFlags, memp ret_fd);
+void _target_openf(const string path, uint oFlags, memp ret_fd);
 
-status _target_readf(const memp fd, memp ret_mut_buff,
-                   const memsz nbytes_towrite, memsz ret_nbytes_written);
-status _target_writef();
-status _target_cosef();
+void _target_readf(const memp fd, memp ret_mut_buff,
+                   const memsz nbytes_toread, memsz ret_nbytes_written);
+void _target_writef();
+void _target_cosef();
 
 #endif // SRC_SYS_TARGETS__REQUIREMENTS_C
