70ef2ee2c560f3585a3e9f8e375b1a4cf56d9831
Person <localboxcrox@gmail.com>
2025-07-23 02:21:18 -0400

    stuff

--- src/lib/prelude.c
+++ src/lib/prelude.c
@@ -2,7 +2,7 @@
 #define SRC_LIB_PRELUDE_C
 /* mostly for semantic consistency */
 
-#define list(__type_t) __type_t*
+#define list(__type_t) __type_t *
 
 typedef char byte; // should be tied to sizeof
 
@@ -10,36 +10,31 @@ typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned long ulong;
 
-typedef byte* memp; // mem pointer // not void* to make *arithmetic easier
+typedef byte *memp;  // mem pointer // not void* to make *arithmetic easier
 typedef ulong memsz; // mem sized
 
 typedef list(char) string;
 
 typedef enum _tag_bool {
-    true = 1,
-    false = !true
+  true = 1,
+  false = !true
 } bool; // maybe even __attribute__((__packed__)) bool?;
 
 // graceful error handeling
-#define _def_result(name, etc) \
-    typedef struct __result##name {\
-        bool ok;\
-        etc;\
-    } result##name
+#define _def_result(name, etc)                                                 \
+  typedef struct __result##name {                                              \
+    bool ok;                                                                   \
+    etc;                                                                       \
+  } result##name
 
-_def_result(,); // result
-const result RESOK = {
-    .ok = true
-};
-const result RESERR = {
-    .ok = false
-};
+_def_result(, ); // result
+const result RESOK = {.ok = true};
+const result RESERR = {.ok = false};
 
 _def_result(_str, string more); // result_str
 
 // result complient functions
-#define _defn(name, _ret_t, ivar)\
-    _ret_t* fn_##name(_ret_t* ivar)
+#define _defn(name, _ret_t, ivar) _ret_t *fn_##name(_ret_t *ivar)
 
 /* example */
 // _defn(b, result_str, s) {
@@ -52,4 +47,4 @@ _def_result(_str, string more); // result_str
 //     bool lol = fn_b(&nigma)->ok;
 // }
 
-#endif //SRC_LIB_PRELUDE_C
+#endif // SRC_LIB_PRELUDE_C
