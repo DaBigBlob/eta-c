e277ed46f48c6a775a5c7e340518c97159ff082b
Person <localboxcrox@gmail.com>
2025-07-23 04:51:26 -0400

    stuff

--- src/sys/_.c
+++ src/sys/_.c
@@ -1,12 +1,13 @@
 #ifndef SRC_SYS___C
 #define SRC_SYS___C
 
+// TODO: ifdef chain for each triplet
+
 #ifndef NEWLINE
     #define NEWLINE "\n"
 #endif // NEWLINE
 
 #define nl NEWLINE
 
-// TODO: ifdef chain for each triplet
- 
+
 #endif //SRC_SYS___C
