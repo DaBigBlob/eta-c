d75d56516c38dc727a6b2491d2b1da32801b9209
Person <localboxcrox@gmail.com>
2025-07-22 22:35:23 -0400

    stuff

--- src/lib/prelude.c
+++ src/lib/prelude.c
@@ -6,6 +6,11 @@ typedef enum __bool {
     false = !true
 } bool;
 
+typedef void* memp;
+
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
 
  
 #endif //SRC_LIB_PRELUDE_C
