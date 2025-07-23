f0e3ee5796466a972c59a1280cc4404a77f3cd8c
Person <localboxcrox@gmail.com>
2025-07-23 04:38:23 -0400

    stuff

--- src/sys/targets/_requirements.c
+++ src/sys/targets/_requirements.c
@@ -5,22 +5,52 @@
 
 #include "../../lib/prelude.c"
 
-deft(zerot, _target_unpack_args, struct {uint argc_p; list(string) argv_p;}, zerot)
+deft(
+    zerot,
+    _target_unpack_args,
+    struct {uint argc_p; list(string) argv_p;},
+    zerot
+)
 defn(_target_unpack_args);
 
-deft(uint, __target_exit, zerot, zerot)
-defn(__target_exit);
+deft(
+    uint,
+    _target_exit,
+    zerot,
+    zerot
+)
+defn(_target_exit);
 
-deft(struct {const string path; uint oflags;}, _target_openf, memp, const string)
+deft(
+    struct {const string path; uint oflags;},
+    _target_openf,
+    struct {const memp fd;},
+    const string
+)
 defn(_target_openf);
 
-deft(struct {const memp fd; memp wbuff; const memsz nbytes_to_read;}, _target_readf, memsz, const string)
+deft(
+    struct {const memp fd; memp wbuff; const memsz nbytes_to_read;},
+    _target_readf,
+    struct {const memsz nbytes_read;},
+    const string
+)
 defn(_target_readf);
 
-deft(struct {memp fd; const memp rbuff; const memsz nbytes_to_write;}, _target_writef, memsz, const string)
+deft(
+    struct {memp fd; const memp rbuff; const memsz nbytes_to_write;},
+    _target_writef,
+    struct {const memsz nbytes_written;},
+    const string
+)
 defn(_target_writef);
 
-deft(memp, _target_cosef, zerot, const string)
+deft(
+    struct {const memp fd;},
+    _target_cosef,
+    zerot,
+    const string
+)
 defn(_target_cosef);
 
 #endif // SRC_SYS_TARGETS__REQUIREMENTS_C
