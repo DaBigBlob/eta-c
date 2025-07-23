43d56b16c5c622c25ec71ca7472df55fa177ce3d
Person <localboxcrox@gmail.com>
2025-07-23 02:17:21 -0400

    stuff

--- src/lib/bytes.c
+++ src/lib/bytes.c
@@ -3,7 +3,7 @@
 
 #include "./prelude.c"
 
-#define _def_byte_delimit_sizeof_buff(delim, buff) {for(memp b=buff;;b++) if (*b==delim) return b-buff;}
+#define _def_byte_delimit_sizeof_buff(delim, buff) {for(memp b=buff;;b++) if (*b==delim) return b-buff;} // not including delim
 
 inline memsz sizeof_delim_buff(byte delim, const memp buff) _def_byte_delimit_sizeof_buff(delim, buff)
 inline memsz sizeof_buff(const memp buff) _def_byte_delimit_sizeof_buff('\0', buff)
