86162da1833054fb3872143789b07121ccae5b77
Person <localboxcrox@gmail.com>
2025-07-24 01:09:35 -0400

    achieve hello world

--- src/main.c
+++ src/main.c
@@ -2,11 +2,22 @@
 #define SRC_MAIN_C
 
 #include "./lib/prelude.c"
+#include "./lib/bytes.c"
+#include "./sys/_.c"
 
 uint sys_main(uint argc, list(string) argv) {
     
     // hey clang, how about you shut the fuck up?
     if (argc) if (argv) {}
+
+    _target_writef_data wdata = {
+        .in = {
+            .fd = STDOUT_FD,
+            .rbuff = "Hello world!" nl "\0",
+            .nbytes_to_write = sizeof_buff(wdata.in.rbuff)
+        }
+    };
+    _target_writef_fn(&wdata);
 
     return 0;
 }
