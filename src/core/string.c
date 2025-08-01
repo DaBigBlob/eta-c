1bdf6dab2d18f645223fdb406692c56ca85cb153
Person <localboxcrox@gmail.com>
2025-08-01 05:03:08 -0400

    middle of refactoring; very broken

--- src/core/string.c
+++ src/core/string.c
@@ -7,14 +7,17 @@ Utility functions to deal with (byte) strings.
 
 #include "./prelude.c"
 
+/// returns length of buffer (excluding byte delimitor)
 #define _def_byte_delimit_sizeof_buff(delim, buff) {\
     for (memp b = buff;; b++) if (*b == delim) return b - buff;\
-} // not including delim
+}
 
+/// length of arbitary byte delimited string
 inline memsz
 sizeof_delim_buff(byte delim, const memp buff)
 _def_byte_delimit_sizeof_buff(delim, buff)
 
+/// length of \0 delimited string
 inline memsz
 sizeof_buff(const memp buff)
 _def_byte_delimit_sizeof_buff('\0', buff)
