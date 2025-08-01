82b6d5da67e1762ddca5110ee7c67fa677e647c6
Person <localboxcrox@gmail.com>
2025-08-01 04:57:10 -0400

    middle of refactoring; very broken

--- src/core/bytes.c
+++ src/core/bytes.c
@@ -1,18 +1,4 @@
 #ifndef SRC_CORE_BYTES_C
 #define SRC_CORE_BYTES_C
-
-#include "./prelude.c"
-
-#define _def_byte_delimit_sizeof_buff(delim, buff) {\
-    for (memp b = buff;; b++) if (*b == delim) return b - buff;\
-} // not including delim
-
-inline memsz
-sizeof_delim_buff(byte delim, const memp buff)
-_def_byte_delimit_sizeof_buff(delim, buff)
-
-inline memsz
-sizeof_buff(const memp buff)
-_def_byte_delimit_sizeof_buff('\0', buff)
-
+ 
 #endif //SRC_CORE_BYTES_C
