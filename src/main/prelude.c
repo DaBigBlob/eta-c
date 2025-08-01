b0737648aacafd3194625b6f8b4be0ff8d99b13c
Person <localboxcrox@gmail.com>
2025-08-01 05:14:29 -0400

    middle of refactoring; very broken

---
+++ src/main/prelude.c
@@ -0,0 +1,10 @@
+#ifndef SRC_MAIN_PRELUDE_C
+#define SRC_MAIN_PRELUDE_C
+
+/*
+This file exists for flatness like src/core.
+*/
+
+#include "../core/prelude.c" // IWYU pragma: keep
+
+#endif //SRC_MAIN_PRELUDE_C
---
+++ src/sys/prelude.c
@@ -0,0 +1,10 @@
+#ifndef SRC_SYS_PRELUDE_C
+#define SRC_SYS_PRELUDE_C
+
+/*
+This file exists for flatness like src/core.
+*/
+
+#include "../core/prelude.c" // IWYU pragma: keep
+ 
+#endif //SRC_SYS_PRELUDE_C
