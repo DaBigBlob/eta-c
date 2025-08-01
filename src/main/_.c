0da3d72a4e3d1c0ed07c254792afa2bd2cbabdff
Person <localboxcrox@gmail.com>
2025-08-01 05:56:20 -0400

    middle of refactoring; very broken

--- src/main/_.c
+++ src/main/_.c
@@ -6,6 +6,7 @@ Entry for compiler.
 */
 
 #include "../core/prelude.c" // IWYU pragma: keep
+#include "./main.c" // IWYU pragma: keep
 
 uint sys_main(uint argc, list(string) argv) __asm__ ("__sys_main");
 
