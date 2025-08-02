fa2f636017b30094518e366b35ef295e69033505
Person <localboxcrox@gmail.com>
2025-08-02 13:19:54 -0400

    middle of refactoring; very broken

--- src/lib/rprcalloc.c
+++ src/lib/rprcalloc.c
@@ -19,16 +19,32 @@ typedef struct {
     memsz size;
 } rprc_block_desc;
 
+#ifndef RPRC_TOLERANCE
+    /// max number of extra bytes the allocator may freely assign
+    #define RPRC_TOLERANCE 4
+#endif // RPRC_TOLERANCE
+
 /*
 In the act of allocating a new block with reference R and size S,
     - for each block already present with reference aR and size aS,
         - check if &aR == aR i.e. reverse pointer check
             - alive -> repeat for next block
-            - dead -> check size of the block for aS >= S + sizeof(rprc_block_desc)
-                - true -> 
-                - fale ->
+            - dead -> check size of the block for aS >= S && aS <= S + RPRC_TOLERANCE
+                - true -> re-assign block
+                - fale -> set remember block as has_potetial,
+                  check subsequent blocks' sizes to see if aS >= S && aS <= S + RPRC_TOLERANCE
+                  and &aR != aR criteria is met
+                    - yes -> re-assign the first block descriptor with total
+                             size calculated and reference reference replaced
+                    - no -> check next block as regular
     - if not block present i.e. end sentinal,
         - allocate new
+*/
+
+/*
+rprcalloc usage:
+something* new_ptr;
+rprcalloc(&(memp)new_ptr, sizeof(something), arena);
 */
 
 void rprcalloc(memp* refp, memsz size, memp arena) {
